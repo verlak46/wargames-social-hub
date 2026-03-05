cb9fb9d4b3e4395774fab1e40bfcdc1d94267227
ivan.easypyme <ivan.easypyme@gmail.com>
2026-03-05 15:05:20 +0100

    fix: update Firestore query to filter active wargames in getActiveWargames method

--- src/app/core/services/firestore.service.ts
+++ src/app/core/services/firestore.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Wargame } from '../../shared/models/IWargame';
 import { firestore } from '../../app.config';
-import { collection, onSnapshot  } from '@firebase/firestore';
+import { collection, onSnapshot, query, where } from '@firebase/firestore';
 
 @Injectable({ providedIn: 'root' })
 export class FirestoreService {
@@ -10,9 +10,10 @@ export class FirestoreService {
 
   getActiveWargames(): Observable<Wargame[]> {
     const ref = collection(this.firestore, 'wargames');
+    const q = query(ref, where('active', '==', true));
 
     return new Observable(subscriber => {
-      const unsubscribe = onSnapshot(ref, snapshot => {
+      const unsubscribe = onSnapshot(q, snapshot => {
         const games: Wargame[] = snapshot.docs.map(doc => ({
           id: doc.id,
           ...doc.data()
